7098e18c5fd2a069b0ab7ccfc0c1140159096b6a
Niennis <estefania.osses.v@gmail>
2018-03-18 15:08:22 -0300

    Indentacion

--- assets/js/loginpage.js
+++ assets/js/loginpage.js
@@ -1,66 +1,64 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyAh8jLxKg9BALWSaYIiUBk-vXnRa_4gu4I",
-  authDomain: "hackathon-movies.firebaseapp.com",
-  databaseURL: "https://hackathon-movies.firebaseio.com",
-  projectId: "hackathon-movies",
-  storageBucket: "hackathon-movies.appspot.com",
-  messagingSenderId: "215425576358"
+  apiKey: 'AIzaSyAh8jLxKg9BALWSaYIiUBk-vXnRa_4gu4I',
+  authDomain: 'hackathon-movies.firebaseapp.com',
+  databaseURL: 'https://hackathon-movies.firebaseio.com',
+  projectId: 'hackathon-movies',
+  storageBucket: 'hackathon-movies.appspot.com',
+  messagingSenderId: '215425576358'
 };
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-//Registrarse con email
-function create(){
-  
+// Registrarse con email
+function create() {  
   var email2 = document.getElementById('email2').value;
   var password2 = document.getElementById('password2').value;
   firebase.auth().createUserWithEmailAndPassword(email2, password2).catch(function(error) {
   // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  console.log(errorCode);
-  console.log(errorMessage);
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
   // ...
-});
+  });
 }
 
-//Ingreso de usuarios existentes 
-function enter(){
+// Ingreso de usuarios existentes 
+function enter() {
   var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
 
   firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
   // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  console.log(errorCode);
-  console.log(errorMessage);
-  // ...
-});
-
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+    // ...
+  });
 }
 
 // realiza acciones si está o no logeado el usuario
-function  watcher(){
+function watcher() {
   firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    console.log('Existe usuario activo');
-    location.href ="paginicio.html"; //envia a la pagina de inicio solo si se esta logeado
-    // User is signed in.
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    // ...
-  } else {
-    console.log('No existe usuario activo')
+    if (user) {
+      console.log('Existe usuario activo');
+      location.href = 'paginicio.html'; // envia a la pagina de inicio solo si se esta logeado
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
+      console.log('No existe usuario activo')
 
-    // User is signed out.
-    // ...
-  }
-});
+      // User is signed out.
+      // ...
+    }
+  });
 } 
 watcher();
--- assets/js/paginicio.js
+++ assets/js/paginicio.js
@@ -1,53 +1,52 @@
 $(document).ready(function() {
-
-  $('#searchForm').on('submit', (e) => {
+  $('#searchForm').on('submit', (event) => {
     let searchText = $('#searchText').val();
     getMovies(searchText);
-    e.preventDefault();
+    event.preventDefault();
   });
 
   $('.carousel').carousel({
     interval: 4000
-  })
+  });
 });
 
 // INICIO FILTRO PELICULAS
-function getMovies(searchText){
-  axios.get('https://www.omdbapi.com?s='+searchText+'&apikey=b3cb4e7')
-    .then(function(response){
-    //console.log(response);
+function getMovies(searchText) {
+  axios.get('https://www.omdbapi.com?s=' + searchText + '&apikey=b3cb4e7')
+    .then(function(response) {
+    // console.log(response);
       let movies = response.data.Search;
-      //console.log('unicornio', movies);
+      // console.log('unicornio', movies);
       
       var ids = []; 
-      for(var j = 0; j < movies.length ; j++){
+      for (var j = 0; j < movies.length ; j++) {
         ids.push(movies[j].imdbID);
       }
-      //console.log('id', jojo);
+      // console.log('id', jojo);
 
-      for(var k = 0; k< ids.length; k++){
-        var idMovie =[];
+      for (var k = 0; k < ids.length; k++) {
+        var idMovie = [];
         var infoMovie = [];
         var awardedMovie = [];
-        axios.get('https://www.omdbapi.com?i='+ids[k]+'&apikey=b3cb4e7').then(function(response){
-          //console.log('response', response);
+        axios.get('https://www.omdbapi.com?i=' + ids[k] + '&apikey=b3cb4e7').then(function(response) {
+          // console.log('response', response);
           infoMovie.push(response.data); 
-          //console.log ('response data', infoMovie);
-          //console.log('prueba', infoMovie[0].Awards);
+          // console.log ('response data', infoMovie);
+          // console.log('prueba', infoMovie[0].Awards);
 
           idMovie.push(response.data.imdbID);
-          //console.log('ID', idMovie);
+          // console.log('ID', idMovie);
 
-        // Recorremos las películas para seleccionar solo las premiadas o nominadas al Oscar.
+          // Recorremos las películas para seleccionar solo las premiadas o nominadas al Oscar.
           var awardedMovies = [];     
-          for (var m = 0 ; m < infoMovie.length ; m++){
-            if ((infoMovie[m].Awards).indexOf("Oscar") >= 0){
+          for (var m = 0 ; m < infoMovie.length ; m++) {
+            if ((infoMovie[m].Awards).indexOf('Oscar') >= 0) {
               awardedMovies.push(infoMovie[m]);
-              //console.log('jojo',infoMovie); retorna todas las pelis consultadas
-              //console.log('awardededed', awardedMovies);
+              // console.log('jojo',infoMovie); retorna todas las pelis consultadas
+              // console.log('awardededed', awardedMovies);
 
               let output = '';
-              $.each(awardedMovies, function(index, movie){
+              $.each(awardedMovies, function(index, movie) {
                 output += `
                   <div class="col-md-3 col-xs-3 col-lg-3 col-sm-3">
                     <div class="well text-center">
@@ -59,32 +58,32 @@ function getMovies(searchText){
                 `;
               });
               $('#movies').html(output);
-          }else{
+            } else {
               console.log('none');
-          }   
-        }
-      })      
-    }
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+            }   
+          }
+        });      
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
-function movieSelected(id){
+function movieSelected(id) {
   sessionStorage.setItem('movieId', id);
   window.location = 'moviepage.html';
   return false;
 }
 
-function getMovie(){
+function getMovie() {
   let movieId = sessionStorage.getItem('movieId');  
-  axios.get('https://www.omdbapi.com?i='+movieId+'&apikey=b3cb4e7')
-    .then(function(response){
+  axios.get('https://www.omdbapi.com?i=' + movieId + '&apikey=b3cb4e7')
+    .then(function(response) {
       console.log(response);
       let movie = response.data;
 
-      let output =`
+      let output = `
         <div class="row">
           <div class="col-md-4">
           <ul>
@@ -105,7 +104,7 @@ function getMovie(){
               <li class="list-group-item"><strong>Plot:</strong> ${movie.Plot}
               <hr>
               <a href="https://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-              <a href="index.html" class="btn btn-default">Go Back To Search</a></li>
+              <a href="paginicio.html" class="btn btn-default">Go Back To Search</a></li>
               
             </ul>
           </div>
@@ -114,7 +113,7 @@ function getMovie(){
 
       $('#movie').html(output);
     })
-    .catch(function(err){
+    .catch(function(err) {
       console.log(err);
     });
 }
@@ -123,89 +122,89 @@ function getMovie(){
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyAh8jLxKg9BALWSaYIiUBk-vXnRa_4gu4I",
-  authDomain: "hackathon-movies.firebaseapp.com",
-  databaseURL: "https://hackathon-movies.firebaseio.com",
-  projectId: "hackathon-movies",
-  storageBucket: "hackathon-movies.appspot.com",
-  messagingSenderId: "215425576358"
+  apiKey: 'AIzaSyAh8jLxKg9BALWSaYIiUBk-vXnRa_4gu4I',
+  authDomain: 'hackathon-movies.firebaseapp.com',
+  databaseURL: 'https://hackathon-movies.firebaseio.com',
+  projectId: 'hackathon-movies',
+  storageBucket: 'hackathon-movies.appspot.com',
+  messagingSenderId: '215425576358'
 };
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 
-//ejecuta acciones dependiendo si el user esta o no logeado
-function watcher(){
+// ejecuta acciones dependiendo si el user esta o no logeado
+function watcher() {
   firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    console.log('Existe usuario activo');
-        // User is signed in.
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    // ...
-  } else {
+    if (user) {
+      console.log('Existe usuario activo');
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
     // User is signed out.
-    /*location.reload();*/
-    console.log('No hay usuario');
-    location.href ="loginpage.html"; //vuelve a la pagina de inicio si se deslogea
+    /* location.reload();*/
+      console.log('No hay usuario');
+      location.href = 'loginpage.html'; // vuelve a la pagina de inicio si se deslogea
     // ...
-  }
-});
-}
- watcher();
-
-//deslogear
-function getOut(){
-  firebase.auth().signOut()
-  .then(function(){
-    console.log('Saliendo....')
-  })
-  .catch(function(error){
-    console.log(error)  
-  })
-  location.reload(); //actualiza para que al deslogear desaparesca Profile y Log out 
-}
-
-  // INICIO RATING
-  $('.stars').on('click', '.star', function() {
-    $(this).siblings('.pick').removeClass('pick');
-    $(this).addClass('pick');
+    }
   });
-  // FIN RATING  
+}
+watcher();
 
-  // INICIO THUMBS
-  var up = $('.thumbsUp');
-  var down = $('.thumbsDown');
-
-  up.click(function(){
-    $(this).toggleClass('orangeColor');
-    down.removeClass('yellowColor');
-  })
-  down.click(function(){
-    $(this).toggleClass('yellowColor');
-    up.removeClass('orangeColor');
-  })
-  // FIN THUMBS
-
-  // INICIO AGREGAR COMENTARIOS
-
-  $('#btn').click(function(){
-    var comentario = $('#comment').val();
-    $('#comment').val("");
-    var contenedor = $('#cont');
-    contenedor.prepend('<div class="col-md-8 col-md-offset-4 colPost"><span class="nameUser">Vale </span>' + 
-                       '<span class="said"> said...</span>' + '<span class="datePost">' + moment().format("MMM Do YY, H:mm:ss") + '</span>' + 
-                       '<p class="textPost">' + comentario + 
-                       '</p><i class="fa fa-trash trash"></i><i class="fa fa-heart heart"></i></div>');
-    $('.heart').click(function(){
-      $(this).toggleClass('redColor');
-    });
-    $('.trash').click(function(){
-      $(this).parent().remove();
+// deslogear
+function getOut() {
+  firebase.auth().signOut()
+    .then(function() {
+      console.log('Saliendo....');
     })
-  }); 
-  // FIN AGREGAR COMENTARIOS
+    .catch(function(error) {
+      console.log(error);  
+    });
+  location.reload(); // actualiza para que al deslogear desaparesca Profile y Log out 
+}
+
+// INICIO RATING
+$('.stars').on('click', '.star', function() {
+  $(this).siblings('.pick').removeClass('pick');
+  $(this).addClass('pick');
+});
+// FIN RATING  
+
+// INICIO THUMBS
+var up = $('.thumbsUp');
+var down = $('.thumbsDown');
+
+up.click(function() {
+  $(this).toggleClass('orangeColor');
+  down.removeClass('yellowColor');
+});
+down.click(function() {
+  $(this).toggleClass('yellowColor');
+  up.removeClass('orangeColor');
+});
+// FIN THUMBS
+
+// INICIO AGREGAR COMENTARIOS
+
+$('#btn').click(function() {
+  var comentario = $('#comment').val();
+  $('#comment').val('');
+  var contenedor = $('#cont');
+  contenedor.prepend('<div class="col-md-8 col-md-offset-4 colPost"><span class="nameUser">Vale </span>' + 
+                      '<span class="said"> said...</span>' + '<span class="datePost">' + moment().format('MMM Do YY, H:mm:ss') + '</span>' + 
+                      '<p class="textPost">' + comentario + 
+                      '</p><i class="fa fa-trash trash"></i><i class="fa fa-heart heart"></i></div>');
+  $('.heart').click(function() {
+    $(this).toggleClass('redColor');
+  });
+  $('.trash').click(function() {
+    $(this).parent().remove();
+  });
+}); 
+// FIN AGREGAR COMENTARIOS
